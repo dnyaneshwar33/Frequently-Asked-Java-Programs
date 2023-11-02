93b83f82c53377a184a629ca80c5797dc87f1f97
Dnyaneshwar Vighne <125205721+dnyaneshwar33@users.noreply.github.com>
2023-11-02 09:26:43 +0530

    Add files via upload

---
+++ pract.java
@@ -0,0 +1,32 @@
+//program to check number is even or odd
+// class pract
+// {
+//     public static void main(String a[])
+//     {
+//         int i;
+//         for(i=0;i<=100;i++)
+//         {
+//             if(i%2!=0)
+//             {
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+// }
+
+//program to print pattern
+class pract{
+    public static void main(String a[])
+    {
+        int i,j,n=5;
+        for(i=0;i<5;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
